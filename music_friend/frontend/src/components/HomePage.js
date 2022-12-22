134b0762f2a311ae52c6e706a322293aceaaa030
Linda Wang <61102300+ylw311@users.noreply.github.com>
2022-12-21 21:19:08 -0500

    changed switch to routes

---
+++ music_friend/frontend/src/components/HomePage.js
@@ -0,0 +1,28 @@
+import React, { Component } from "react";
+import JoinRoomPage from "./JoinRoomPage";
+import CreateRoomPage from "./CreateRoomPage";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link,
+  Redirect,
+} from "react-router-dom";
+
+export default class HomePage extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Router>
+        <Routes>
+          <Route path="/" element={<p>This is the HomePage</p>} />
+          <Route path="/join/*" element={<JoinRoomPage />} />
+          <Route path="/create" element={<CreateRoomPage />} />
+        </Routes>
+      </Router>
+    );
+  }
+}
